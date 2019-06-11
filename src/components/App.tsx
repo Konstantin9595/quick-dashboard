9c3bcf35796e90b08f7ddffffe2ff9aec2efb2f4
Konstantin <konstantins9595@gmail.com>
2019-06-11 14:04:08 +0600

    created project env

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import '../styles/App.scss';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Layout from './Layout'
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,194 @@
+import React, { Fragment } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+// import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+// import Link from '@material-ui/core/Link';
+
+
+const useStyles = makeStyles(theme => ({
+    appBar: {
+      justifyContent: 'space-between'
+    },
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: 120,
+            '&:focus': {
+                width: 200,
+            },
+        },
+    },
+    list: {
+        textAlign: 'left'
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const Layout: React.FC = () => {
+    const classes = useStyles();
+
+    return (
+        <Fragment>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar className={classes.appBar}>
+                    <Typography variant="h6" color="inherit" noWrap>
+                        Quick dashboard
+                    </Typography>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Поиск…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </div>
+                </Toolbar>
+            </AppBar>
+            <main>
+                {/* Hero unit */}
+                <div className={classes.heroContent}>
+                    <Container maxWidth="sm">
+                        <Typography component="h3" variant="h3" align="center" color="textPrimary" gutterBottom>
+                            Quick dashboard for frontender
+                        </Typography>
+                        <div className="desc">
+                            <p>
+                                <q>
+                                    Оснавная идея заключается в быстром доступе к документации самых популярных библиотек
+                                    для фронтендера.
+                                </q>
+                            </p>
+                            <div>
+                                <h2>Как это работает:</h2>
+                                <ul className={classes.list}>
+                                    <li>Работа происходит с gitHub GraphQL API queries.</li>
+                                    <li>Запрашиваются самые популярные javaScript библиотеки и выводятся в порядке убывания.</li>
+                                    <li>Имеется возможность фильтрации</li>
+                                    <li>Имеется возможность поиска по ключевым словам</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </Container>
+                </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                <Grid>Filter</Grid>
+                    {/* End hero unit */}
+                    <Grid container spacing={4}>
+                        {cards.map(card => (
+                            <Grid item key={card} xs={12} sm={6} md={4}>
+                                <Card className={classes.card}>
+                                    <CardMedia
+                                        className={classes.cardMedia}
+                                        image="https://source.unsplash.com/random"
+                                        title="Image title"
+                                    />
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Heading
+                                        </Typography>
+                                        <Typography>
+                                            This is a media card. You can use this section to describe the content.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small" color="primary">
+                                            View
+                                        </Button>
+                                        <Button size="small" color="primary">
+                                            Edit
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+            {/* Footer */}
+            <footer className={classes.footer}>
+                <Typography variant="h6" align="center" gutterBottom>
+                    Footer
+                </Typography>
+                <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+                    Something here to give the footer a purpose!
+                </Typography>
+            </footer>
+            {/* End footer */}
+        </Fragment>
+    );
+}
+
+export default Layout
